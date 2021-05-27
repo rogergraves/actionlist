7f4ec587643afd4e2ab9100949f7b0f358716a95
Penguin God <56172801+penguin-hacks@users.noreply.github.com>
2021-05-27 15:49:27 -0700

    Create User Model

---
+++ app/models/user.rb
@@ -0,0 +1,13 @@
+class User < ApplicationRecord
+    validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP } 
+    validates :slug, presence: true, uniqueness: true
+    validate :slug_validation
+
+    private
+
+    def slug_validation
+        if slug.match(/\W/)
+            errors.add(:slug, "must be lowercase alphanumeric")
+        end
+    end
+end
