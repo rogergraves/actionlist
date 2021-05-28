e77214f1ff65878309b5658047a485896af81dfb
Penguin God <56172801+penguin-hacks@users.noreply.github.com>
2021-05-28 15:49:57 -0700

    Cleaned up user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,14 +1,4 @@
 class User < ApplicationRecord
     include Slugable
     validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP } 
-    # validates :slug, presence: true, uniqueness: true
-    # validate :slug_validation
-
-    private
-
-    def slug_validation
-        if slug.match(/\W/)
-            errors.add(:slug, "must be lowercase alphanumeric")
-        end
-    end
 end
