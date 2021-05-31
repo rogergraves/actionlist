49e788fbd3c68bc5192528a73b599545f800bae3
Penguin God <56172801+penguin-hacks@users.noreply.github.com>
2021-05-31 11:52:08 -0700

    Fixed up code

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,13 @@
 class User < ApplicationRecord
     validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP } 
     after_create :add_slug
+
+    private
       
     def add_slug
         return unless slug.nil?
 
         string_length = (ENV['SLUG_LENGTH'] || 10).to_i
         self.update(slug: SecureRandom.hex[0..string_length])
-        return unless respond_to?(slug)
-        return if send(slug).present?
     end
 end
