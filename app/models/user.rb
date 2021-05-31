e28e96c31af63c3c26649cefe2054ec7506ee37a
Penguin God <56172801+penguin-hacks@users.noreply.github.com>
2021-05-31 11:23:39 -0700

    pen-24-rewrite-the-slugable-code

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,15 @@
 class User < ApplicationRecord
-    include Slugable
     validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP } 
+    after_create :add_slug
+      
+        private
+      
+    def add_slug
+        return unless slug.nil?
+
+        string_length = (ENV['SLUG_LENGTH'] || 10).to_i
+        self.update(slug: SecureRandom.hex[0..string_length])
+        return unless respond_to?(slug)
+        return if send(slug).present?
+    end
 end
