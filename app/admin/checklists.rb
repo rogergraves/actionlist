03a54248f0d800741916a16da5a340d7e22acd51
Penguin God <56172801+penguin-hacks@users.noreply.github.com>
2021-06-04 15:30:26 -0700

    Made activeadmin page easier to use

--- app/admin/checklists.rb
+++ app/admin/checklists.rb
@@ -6,6 +6,36 @@ ActiveAdmin.register Checklist do
   # Uncomment all parameters which should be permitted for assignment
   #
   permit_params :name, :call_to_action, :email_when_finished
+
+  index do
+    id_column
+    column "Name Link" do |checklist|
+      link_to "#{checklist.name}", "/admin/checklists/#{checklist.id}"
+    end
+    column :call_to_action
+    column :email_when_finished
+    actions
+  end
+
+  show do
+    panel "Checklist" do
+      table_for checklist do
+        column :call_to_action
+        column :email_when_finished
+      end
+
+      panel "Items" do
+        table_for checklist.items do
+          column :description
+          column :order
+          column "Item Link" do |item|
+            link_to 'Item', "/admin/items/#{item.id}"
+          end
+        end
+      end
+    end
+    active_admin_comments
+  end
   #
   # or
   #
