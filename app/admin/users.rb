04d75564a31fe12467e993ba5d48e668c2da264a
Penguin God <56172801+penguin-hacks@users.noreply.github.com>
2021-07-14 16:47:52 -0500

    commit

--- app/admin/users.rb
+++ app/admin/users.rb
@@ -6,6 +6,32 @@ ActiveAdmin.register User do
   # Uncomment all parameters which should be permitted for assignment
   #
   permit_params :email
+
+  show title: 'Users' do |user|
+    attributes_table do
+      row :email
+      row :slug
+      row :created_at
+      row :updated_at
+      row 'Checklists' do
+        all_checklists = Checklist.all
+        list = ""
+        all_checklists.each do |checklist|
+          item_ids = user.items.pluck(:item_id)
+          if Item.where(id: item_ids, checklist_id: checklist.id).any?
+            list += "<h2>#{checklist.name}</h2>"
+            list += "<ul>"
+            checklist.items.each do |item|
+              list += "<li> #{item.description} </li>" if UserItem.find_by(item_id: item.id, user_id: user.id, checked: true).present?
+            end
+            list += "</ul>"
+          end
+        end
+        list.html_safe
+      end
+      # row :items_checked
+    end
+  end
   #
   # or
   #
